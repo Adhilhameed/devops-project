46be4c45ff3e90a675f35f77ab86c707c5299838
Adhil Hameed <adhilkhaliq@gmail.com>
2026-02-24 15:32:28 +0530

    Fix syntax error in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-:import "./App.css";
+import "./App.css";
 
 function App() {
   return (
